317c5d83f1ff28ca912fbeb5bc90ed566fda140e
Tunsha <tunshabenjamin@gmail.comclear>
2023-08-12 07:10:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point 
- * Return: 0 (Success) 
+ * main - A program that prints a line with puts function
+ * Return: 0(Success)
  */
 int main(void)
 {
-	puts("\"programing is like building a multilingual puzzle");
-	return (0);
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
