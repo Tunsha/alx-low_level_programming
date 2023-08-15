35195bc097f9c128e0006b0568b0073b3cba3502
Tunsha <tunshabenjamin@gmail.comclear>
2023-08-14 20:31:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
+ * main - Entry point
  *
- * Return: Always  0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
