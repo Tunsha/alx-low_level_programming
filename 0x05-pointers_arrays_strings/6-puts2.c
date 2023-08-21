0d1488659f2a6f01b06b7eb326823edf48ac976b
Tunsha <tunshabenjamin@gmail.comclear>
2023-08-21 03:23:07 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string
+ * puts2 - Prints every other character of a string
  * @str: The string to be treated
  * Return: void
  */
@@ -17,7 +17,7 @@ void puts2(char *str)
 	j++;
 	}
 
-	for (i = 0; i < j; i <= 2)
+	for (i = 0; i < j; i += 2)
 	{
 	_putchar(str[i]);
 	}
